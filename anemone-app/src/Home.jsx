c10601a6402bffb6c24e33a0731bfa292b9c8b2f
Fay Johnston <swewrozka@gmail.com>
2022-04-08 19:32:39 -0400

    add rainbowbrain to dial update homepage link api v1 doc add issue collector add new package libraries

--- anemone-app/src/Home.jsx
+++ anemone-app/src/Home.jsx
@@ -1,9 +1,10 @@
-// import './App.css';
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import SpeedDial from '@mui/material/SpeedDial';
 import SpeedDialAction from '@mui/material/SpeedDialAction';
 import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import rainbowBrain from './image/Anemone_03_b.png';
+
 // import './Collect';
 // import './Organize';
 // import './Modify';
@@ -16,21 +17,22 @@ export default function OpenHomeIconSpeedDial(){
     //     { icon: <ModifyIcon />, name: 'Modify'},
     //     { icon: <RetrieveIcon />, name: 'Retrieve'},
     // ];
-
+    const [direction, setDirection] = React.useState('right');
     const [actions, setActions] = React.useState([]);
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
     
     return(
-        <Box sx={{ height: 320, transform: 'translateZ(0px)', flexGrow: 1 }}>
+        <Box sx={{ height: 120, transform: 'translateZ(0px)', flexGrow: 1 }}>
             <SpeedDial
             ariaLabel="Anemone Rainbow Brain"
-            sx={{ position: 'absolute', bottom: 6, right: 6 }}
-            icon={<SpeedDialIcon />}
+            sx={{ position: 'absolute', right: 6 }}
+            icon={<SpeedDialIcon icon={<img src={rainbowBrain} className="" alt=""/>} />}
             onClose={handleClose}
             onOpen={handleOpen}
             open={open}
+            direction={direction}
             >
             {actions.map((action) => (
                 <SpeedDialAction
