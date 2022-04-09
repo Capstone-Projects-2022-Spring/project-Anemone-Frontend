b6c4af1eb5683892133a6e20b3190e370dff4967
faith-ima <77637834+faith-ima@users.noreply.github.com>
2022-04-09 15:21:26 -0400

    added features component

---
+++ anemone-app/src/components/feature.jsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import RainbowBrain from './image/anemone-03@1x.png';
+import CollectIcon from './collectFeature';
+
+export default function FeatureSpeedDial() {
+    const actions = [
+         { icon: <CollectIcon />, name: 'collectFeature'},
+    ];
+    const [direction, setDirection] = React.useState('right');
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    return (
+        <Box sx={{ height: 120, transform: 'translateZ(0px)', flexGrow: 1 }}>
+            <SpeedDial
+                ariaLabel="Anemone Rainbow Brain"
+                sx={{ position: 'absolute', right: 6 }}
+                icon={<SpeedDialIcon icon={<img src={RainbowBrain} className="" alt="" />} />}
+                onClose={handleClose}
+                onOpen={handleOpen}
+                open={open}
+                direction={direction}
+            >
+                {actions.map((action) => (
+                    <SpeedDialAction
+                        key={action.name}
+                        icon={action.icon}
+                        tooltipTitle={action.name}
+                    />
+                ))}
+            </SpeedDial>
+        </Box>
+    );
+}
