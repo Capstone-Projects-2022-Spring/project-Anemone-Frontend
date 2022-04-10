afdd7cca9260c347dbe50a2c1dd49941a02bda3d
ChenxuanSun123 <70704540+ChenxuanSun123@users.noreply.github.com>
2022-04-09 21:42:00 -0400

    SearchPages upload
    SearchPages upload

---
+++ anemone-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import SearchBar from "./Components/SearchBar";
+import BookData from "./Data.json";
+
+function App() {
+  return (
+    <div className="App">
+      <SearchBar placeholder="Enter the key words: " data={BookData} />
+    </div>
+  );
+}
+
+export default App;
