f29eba69b4148d1a4acca3ec875545c42f07db2e
faith-ima <77637834+faith-ima@users.noreply.github.com>
2022-04-18 12:46:09 -0400

    added register page

--- anemone-app/src/components/Register.jsx
+++ anemone-app/src/components/Register.jsx
@@ -1,140 +1,99 @@
-import React, { Component } from "react";
-import { Button, Form, FormGroup, Label, Input } from "reactstrap";
-import axios from "axios";
+import React from 'react'
 import "./Register.css";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import { useState } from 'react';
+import axios from 'axios';
 import Anemone from "../image/anemone.png"
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+
+const Register = () => {
 
 
-export default class Register extends Component {
-    userData;
-    constructor(props) {
-        super(props);
-        this.state = {
-            registerData: {
-                name: "",
-                email: "",
-                password: "",
-                password2: "",
-                isLoading: "",
-            },
-            msg: "",
-        };
+    const [errors, setErrors] = useState('');
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        password1: "",
+        password2: ""
+    });
+
+    const { name, email, password1, password2 } = user;
+    const onInputChange = e => {
+        setUser({ ...user, [e.target.name]: e.target.value });
+    };
+
+    async function Register() {
+        let result = await axios.post("http://localhost:8000/api/register", user);
+        setErrors('Registration Successful')
+        setUser({ name: "", email: "", password: "" }) // To Clear all fields
+
     }
 
-    onChangehandler = (e, key) => {
-        const { registerData } = this.state;
-        registerData[e.target.name] = e.target.value;
-        this.setState({ registerData });
-    };
-    onSubmitHandler = (e) => {
-        e.preventDefault();
-        this.setState({ isLoading: true });
-        axios
-             .post("http://localhost:8000/api/user-signup", this.state.registerData)
-            // .post("http://localhost:3000", this.state.registerData)
-            .then((response) => {
-                this.setState({ isLoading: false });
-                if (response.data.status === 200) {
-                    this.setState({
-                        msg: response.data.message,
-                        registerData: {
-                            name: "",
-                            email: "",
-                            password: "",
-                            password2: "",
-                        },
-                    });
-                    setTimeout(() => {
-                        this.setState({ msg: "" });
-                    }, 2000);
-                }
+    return (
+        <div className="wrapper">
+            <h3>Anemone Sign Up</h3>
+            <section class="section-head">
+                <div class="container">
+                    <div class="imgcontainer">
+                        <img src={Anemone} className="Anemone" alt="" />
+                    </div>
 
-                if (response.data.status === "failed") {
-                    this.setState({ msg: response.data.message });
-                    setTimeout(() => {
-                        this.setState({ msg: "" });
-                    }, 2000);
-                }
-            });
-    };
-    render() {
-        const isLoading = this.state.isLoading;
-        return (
-            <div>
-                        <h3>Anemone Sign Up</h3>
-                        <section class="section-head">
-                            <div class="container">
-                                <div class="imgcontainer">
-                            <img src={Anemone} className="Anemone" alt=""/>
-                                </div>
+                </div>
+            </section>
 
-                            </div>
-                        </section>
+            <Form className="containers shadow">
+                <FormGroup>
+                    <Label for="name">Name </Label>
+                    <Input
+                        type="name"
+                        name="name"
+                        placeholder="Enter name"
+                        value={name}
+                        onChange={e => setUser(e.target.value)}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="email">Email id </Label>
+                    <Input
+                        type="email"
+                        name="email"
+                        placeholder="Enter email"
+                        value={email}
+                        onChange={e => setUser(e.target.value)}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="password">Password </Label>
+                    <Input
+                        type="password"
+                        name="password"
+                        placeholder="Enter password"
+                        value={password1}
+                        onChange={e => setUser(e.target.value)}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="password2">Re-Enter Password </Label>
+                    <Input
+                        type="password"
+                        name="password2"
+                        placeholder="Re-Enter password"
+                        value={password2}
+                        onChange={e => setUser(e.target.value)}
+                    />
+                </FormGroup>
 
-                <Form className="containers shadow">
-                    <FormGroup>
-                        <Label for="name">Name </Label>
-                        <Input
-                            type="name"
-                            name="name"
-                            placeholder="Enter name"
-                            value={this.state.registerData.name}
-                            onChange={this.onChangehandler}
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="email">Email id </Label>
-                        <Input
-                            type="email"
-                            name="email"
-                            placeholder="Enter email"
-                            value={this.state.registerData.email}
-                            onChange={this.onChangehandler}
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="password">Password </Label>
-                        <Input
-                            type="password"
-                            name="password"
-                            placeholder="Enter password"
-                            value={this.state.registerData.password}
-                            onChange={this.onChangehandler}
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="password2">Re-Enter Password </Label>
-                        <Input
-                            type="password"
-                            name="password2"
-                            placeholder="Re-Enter password"
-                            value={this.state.registerData.password2}
-                            onChange={this.onChangehandler}
-                        />
-                    </FormGroup>
+                <p className="text-white"></p>
+                <div >
+                    <button className="text-center mb-4" type="submit" onSubmit={Register} >Register</button>
+                </div>
+                {/* uncomment when login is made */}
+                {/* <Link to="/login" className="text-white ml-5">Have an account?</Link>  */}
+            </Form>
 
-                    <p className="text-white">{this.state.msg}</p>
-                    <Button
-                        className="text-center mb-4"
-                        color="success"
-                        onClick={this.onSubmitHandler}
-                    >
-                        Register
-                        {isLoading ? (
-                            <span
-                                className="spinner-border spinner-border-sm ml-5"
-                                role="status"
-                                aria-hidden="true"
-                            ></span>
-                        ) : (
-                            <span></span>
-                        )}
-                    </Button>
-                    {/* uncomment when login is made */}
-                    {/* <Link to="/login" className="text-white ml-5">Have an account?</Link>  */}
-                </Form>
-            </div>
-        );
-    }
+        </div>
+
+    );
 }
+
+export default Register
